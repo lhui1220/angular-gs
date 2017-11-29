10671c568e3a1965dfb478d9f2b430778f344db5
liuhui <lhui1220@qq.com>
2017-11-29 15:14:09 +0800

    重构路由到AppRoutingModule模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,
-  	RouterModule.forRoot([
-  		{path: 'dashboard',component: DashboardComponent},
-  		{path: 'heroes',component: HeroesComponent},
-  		{path: 'detail/:id',component: HeroDetailComponent},
-  		{path: '',redirectTo:'/dashboard',pathMatch:'full'} //根路径重定向到Dashboard
-  	])
-   ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
   declarations: [ AppComponent, HeroesComponent,HeroDetailComponent,DashboardComponent ],
   providers:	[ HeroService ],
   bootstrap:    [ AppComponent ]
